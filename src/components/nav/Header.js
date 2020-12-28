3ca03bc302aa768e670adf123a412b798835ce7d
menub <shahrishank@gmail.com>
2020-12-28 17:51:24 +0530

    navbar added

---
+++ src/components/nav/Header.js
@@ -0,0 +1,37 @@
+import React,{useState} from 'react'
+import {Menu} from 'antd'
+import { AppstoreOutlined, SettingOutlined ,UserOutlined, UserAddOutlined} from '@ant-design/icons';
+import {Link} from 'react-router-dom'
+
+const {SubMenu,Item} = Menu
+
+const Header = () =>{
+    const [current,setCurrent] = useState('home')
+
+    const handleClick = (event) =>{
+        setCurrent(event.key);
+    }
+
+    return (
+        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+        <Item key="home" icon={<AppstoreOutlined />}>
+          <Link to="/">Home</Link>
+        </Item>
+
+        <Item key="register" icon={<UserAddOutlined />} className="float-right">
+        <Link to="/login">Login</Link>
+        </Item>
+
+        <Item key="login" icon={<UserOutlined />} className="float-right">
+        <Link to="/register">Register</Link>
+        </Item>
+
+        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+            <Item key="setting:1">Option 1</Item>
+            <Item key="setting:2">Option 2</Item>
+        </SubMenu>
+      </Menu>
+    )
+}
+
+export default Header;
