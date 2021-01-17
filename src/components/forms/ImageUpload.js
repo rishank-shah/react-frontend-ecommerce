198a04d750427ca0c87232332ab3fcb37096a9f2
rishank-shah <shahrishank@gmail.com>
2021-01-17 17:09:15 +0530

    image preview in create product form

--- src/components/forms/ImageUpload.js
+++ src/components/forms/ImageUpload.js
@@ -3,14 +3,47 @@ import resizer from 'react-image-file-resizer'
 import axios from 'axios'
 import {useSelector} from 'react-redux'
 import { toast } from 'react-toastify'
+import {Avatar,Badge} from 'antd'
+import {LoadingOutlined} from '@ant-design/icons'
 
 const ImageUpload = ({values,setValues}) =>{
 
+    const [loading,setLoading] = useState(false)
     const [error,setError] = useState(false)
 
     const {user} = useSelector((state)=>({...state}))
 
+    const handleImageRemove = (public_id)=>{
+        setLoading(true)
+        axios.post(`${process.env.REACT_APP_API_URL}/remove-image`,{
+            public_id
+        },{
+            headers:{
+                authtoken: user?user.token:''
+            }
+        })
+        .then(res=>{
+            if(res.data.message){
+                const {images} = values
+                let filImages = images.filter((image)=>{
+                    return image.public_id !== public_id
+                })
+                setValues({
+                    ...values,
+                    images:filImages
+                })
+                setLoading(false)
+                toast.success(res.data.message)
+            }
+        })
+        .catch(()=>{
+            setLoading(false)
+            toast.error("Failed Image Remove")
+        })
+    }
+
     const uploadAndResize = (e) =>{
+        setLoading(true)
         let files = e.target.files
         let uploadedFiles = values.images
         if(files){
@@ -45,13 +78,23 @@ const ImageUpload = ({values,setValues}) =>{
                 toast.success(`Uploaded images successfully`)
             }
         }
+        setLoading(false)
     }
 
     return(
-        <div className="form-group">
-            <label>Choose Images</label>
-            <input className="form-control" type="file" multiple accept="images/*" onChange={uploadAndResize} />
-        </div>
+        <>
+            <div className="form-group">
+                <span><label>Choose Images</label> {loading ? <LoadingOutlined className="ml-3 text-danger h3"/> : ""} </span>
+                <input className="form-control" type="file" multiple accept="images/*" onChange={uploadAndResize} />
+            </div>
+            <div className="row ml-1 mb-4">
+                {values.images && values.images.map((image)=>(
+                    <Badge key={image.public_id} count="X" onClick={()=>handleImageRemove(image.public_id)} style={{cursor:'pointer'}}>
+                        <Avatar src={image.url} size={100}  shape="square" className="ml-3 mb-3"/>
+                    </Badge>
+                ))}
+            </div>
+        </>
     )
 }
 
